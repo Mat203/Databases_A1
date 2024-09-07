fee935599f986360116ee0e65e59160e5dadb3ba
Mat_Tol <matvey.talalaevskiy@gmail.com>
2024-09-07 21:16:01 +0300

    READ COMMITTED level

--- read.py
+++ read.py
@@ -1,5 +1,4 @@
 import mysql.connector
-import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
 from datetime import datetime
@@ -74,6 +73,53 @@ def read_uncommited_demo():
         if connection2 and connection2.is_connected():
             connection2.close()
 
+def read_commited_demo():
+    """
+    Shows how READ COMMITED isolation level works.
+    Shows NO dirty read.
+    :return: void
+    """
+    connection1 = create_connection()
+    connection2 = create_connection()
+
+    try:
+        cursor1 = connection1.cursor()
+        cursor2 = connection2.cursor()
+
+        # Transaction 1: Read Uncommitted
+        print(f"Transaction 1 started: {datetime.now()}")
+        connection1.start_transaction(isolation_level='READ UNCOMMITTED')
+        cursor1.execute("UPDATE accounts SET balance = 9999 WHERE name = 'Alice'")
+
+        # Transaction 2: Read Committed
+        print(f"Transaction 2 started: {datetime.now()}")
+        connection2.start_transaction(isolation_level='READ COMMITTED')
+        cursor2.execute("SELECT balance FROM accounts WHERE name = 'Alice'")
+        balance_committed_read = cursor2.fetchone()[0]
+
+        print(f"Read (READ COMMITTED): Alice's balance = {balance_committed_read}")
+
+        print(f"Transaction 1 rollback(): {datetime.now()}")
+        connection1.rollback()
+
+        print(f"Transaction 2 commit(): {datetime.now()}")
+        connection2.commit()
+
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        if cursor1:
+            cursor1.close()
+        if connection1 and connection1.is_connected():
+            connection1.close()
+        if cursor2:
+            cursor2.close()
+        if connection2 and connection2.is_connected():
+            connection2.close()
 
 if __name__ == "__main__":
+    print("Demonstrating Dirty Read with READ UNCOMMITTED")
     read_uncommited_demo()
+
+    print("\nDemonstrating No Dirty Read with READ COMMITTED")
+    read_commited_demo()
